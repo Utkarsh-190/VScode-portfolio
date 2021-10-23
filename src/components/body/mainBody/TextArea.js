ede895572beeb8bbb90217aa7a633bb9de725e3c
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-10-23 14:02:16 +0530

    click handlers for changing pages added

--- src/components/body/mainBody/TextArea.js
+++ src/components/body/mainBody/TextArea.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import classes from "./TextArea.module.css";
 import TextAreaHeading from "./TextAreaHeading";
 
 function TextArea(props) {
+  let [page, setPage] = useState("home.jsx");
+
   let fileNames = [
     "home.jsx",
     "about.html",
@@ -11,13 +14,20 @@ function TextArea(props) {
     "github.md",
   ];
 
+  let onClickHandler = (event) => {
+    setPage(event);
+    console.log(page);
+  };
+
   return (
     <div className={`${classes.textArea} ${props.className}`}>
       <div className={classes.headings}>
         {fileNames.map((name) => {
-          return <TextAreaHeading name={name} />;
+          return <TextAreaHeading name={name} onClick={onClickHandler} />;
         })}
       </div>
+
+      {/* {<TextPage page={page} />} */}
     </div>
   );
 }
--- src/components/body/mainBody/TextAreaHeading.js
+++ src/components/body/mainBody/TextAreaHeading.js
@@ -1,7 +1,15 @@
 import classes from "./TextAreaHeading.module.css";
 
 function TextAreaHeading(props) {
-  return <span className={classes.heading}>{props.name}</span>;
+  const handler = (event) => {
+    props.onClick(props.name);
+  };
+
+  return (
+    <span className={classes.heading} onClick={handler}>
+      {props.name}
+    </span>
+  );
 }
 
 export default TextAreaHeading;
